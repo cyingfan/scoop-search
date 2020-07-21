ae42880f073b077010b5474c4f0db7f2fa4feb3d
Ying Fan Chong <yingfan@outlook.com>
2020-07-21 22:15:59 +0800

    Use compression. Improve search state handling

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ApiModule } from './api.module';
 import { join } from 'path';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(ApiModule);
   app.enableCors();
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+  app.use(compression());
   await app.listen(3000);
 }
 bootstrap();
